ed35df6fb85b5e3134226dbccee4343d19e5e619
fsantosbr <fsantosinfo@gmail.com>
2021-04-16 17:46:56 -0300

    Projeto com Angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  title : string = 'frontend';
+  public nome : string;
+  public isDesabilitado : boolean = false;
+
+  public somar() : number{
+    return 1+1;
+  }
+
+  public onMeuBotaoClick(){
+    alert("Mensagem de alerta para teste " + this.title);
+  }
+
+  public capturarDados(valor){
+    console.log(valor);
+    this.nome = valor;
+    // alert("Tecla pressionada");
+  }
 }
